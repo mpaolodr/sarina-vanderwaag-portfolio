c0359b8ae87ed7cabec3ec2691aa66ea98034a05
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-24 18:55:44 -0500

    added basic styling for tldr section, nothing's final

--- src/components/about/Tldr.js
+++ src/components/about/Tldr.js
@@ -8,47 +8,49 @@ const Tldr = () => {
 
         <div className="tldr-statements">
           {/* first row */}
-          <div className="row">
-            <p>I make amazing nail art</p>
-            <p>I make bomb sandwiches</p>
-            <p>I cut my own hair (you may have noticed)</p>
+          <div className="row first">
+            <p className="first-statement">I make amazing nail art</p>
+            <p className="second-statement">I make bomb sandwiches</p>
+            <p className="third-statement">
+              I cut my own hair (you may have noticed)
+            </p>
           </div>
 
           {/* second row */}
-          <div className="row">
-            <p>
+          <div className="row second">
+            <p className="first-statement">
               The sheer beauty of the Itmad-ud-Daula in Agra, India brought me
               to tears (literally)
             </p>
 
-            <div>
-              <p>I am a really good swimmer</p>
-              <p>I'm really a dolphin</p>
+            <div className="flex-container">
+              <p className="second-statement">I am a really good swimmer</p>
+              <p className="third-statement">I'm really a dolphin</p>
             </div>
           </div>
 
           {/* third row */}
-          <div className="row">
+          <div className="row third">
             <p>I am Dutch Antillean</p>
           </div>
 
           {/* fourth row */}
-          <div className="row">
-            <p>I'm sort of a chai connoisseur</p>
-            <p>
+          <div className="row fourth">
+            <p className="first-statement">I'm sort of a chai connoisseur</p>
+            <p className="second-statement">
               I really want to(but don't) play the violin and piano because the
               idea of it is amazing, you must admit
             </p>
           </div>
 
           {/* fifth row */}
-          <div className="row">
-            <p>
+          <div className="row last">
+            <p className="first-statement">
               My favorite books are The Old Man and the Sea, The Storyteller's
               Secret, The White Tiger, The Curious Incident of the Dog in the
               Night-time, The Five People You Meet in Heaven
             </p>
-            <p>I can speak Hindi</p>
+            <p className="second-statement">I can speak Hindi</p>
           </div>
         </div>
       </div>
