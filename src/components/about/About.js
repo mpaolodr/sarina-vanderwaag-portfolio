9a4aee325cf97808b5f325a9db3913fc189c69a7
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-25 13:31:21 -0500

    added aboutme section in about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,12 +3,16 @@ import React from "react";
 //components
 import Profile from "./Profile";
 import Tldr from "./Tldr";
+import AboutMe from "./AboutMe";
 
 const About = () => {
   return (
     <main>
-      <Profile />
-      <Tldr />
+      <div className="indented-container">
+        <Profile />
+        <Tldr />
+        <AboutMe />
+      </div>
     </main>
   );
 };
---
+++ src/components/about/AboutMe.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section className="aboutme-container">
+      <div className="aboutme-content">
+        <div className="title-container">
+          <h2>Have more time?</h2>
+          <p>2 min read</p>
+        </div>
+        <div className="details-container">
+          <p>
+            Design has been a part of my life ever since I could remember. My
+            grandfather was an illustrator for academic research books and my
+            mother was a painter but she didn't have a typical canvas frame to
+            work on. She painted on clothes and created scenes of tropical
+            botany and aves(a.k.a avian dinosaurs, a.k.a birds). Her work was
+            bursting with color, texture, and light. I adored her work and
+            proudly wore the masterpieces.
+          </p>
+
+          <p>
+            As a kid, my bedroom was scattered with paint brushes, coloring
+            pencils, and charcoal. In fact, I can't recall a time in my life
+            when I did not have an easel to create beautiful things on. As I got
+            older, I also dabbed in jewelry making and fashion design. However,
+            my interest wasn't just limited to conventional design. I was
+            attracted to anything that could be made beautiful-plating food for
+            our large family dinners for instance.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
